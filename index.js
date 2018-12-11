7b1eb7e26f6dd10528369509d9e3e9b0ae9309b2
rosen777 <rosen.toshev@flatironschool.com>
2018-12-11 23:17:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    rankedLists[i].innerHTML = parseInt(n + 1)
+  }
+
+  return rankedLists
+
+}
